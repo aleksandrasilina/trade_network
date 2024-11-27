55b778a38381f708480996a212c64f96d482bb63
Александра <aleksandrasilina@gmail.com>
2024-11-27 15:39:36 +0300

    add SellerSerializer to sellers/serializers.py

---
+++ sellers/serializers.py
@@ -0,0 +1,10 @@
+from rest_framework import serializers
+
+from sellers.models import Seller
+
+
+class SellerSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Seller
+        fields = "__all__"
