8c83f68047aadcf123632b80e9b3f445202cb574
Александра <aleksandrasilina@gmail.com>
2024-11-27 14:20:37 +0300

    style files using black, isort

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, path
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
     path("sellers/", include("sellers.urls", namespace="sellers")),
     path("users/", include("users.urls", namespace="users")),
     path("products/", include("products.urls", namespace="products")),
