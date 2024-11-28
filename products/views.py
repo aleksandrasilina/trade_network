f77521b935f7ba9e0c61ca52e4c54e9ef28a2eb3
Александра <aleksandrasilina@gmail.com>
2024-11-28 11:30:24 +0300

    add ProductViewSet to products/views.py,

--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from products.models import Product
+from products.serializers import ProductSerializer
+
+
+class ProductViewSet(ModelViewSet):
+    """Вьюсет для модели продукта."""
+
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
