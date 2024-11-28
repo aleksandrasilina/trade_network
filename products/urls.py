aecd1e46e7d047f2d1a66cea81a54d791064d9c5
Александра <aleksandrasilina@gmail.com>
2024-11-28 11:30:39 +0300

    add ProductViewSet path to products/urls.py,

---
+++ products/urls.py
@@ -0,0 +1,12 @@
+from rest_framework.routers import SimpleRouter
+
+from products.apps import ProductsConfig
+from products.views import ProductViewSet
+
+app_name = ProductsConfig.name
+
+router = SimpleRouter()
+router.register("", ProductViewSet)
+
+urlpatterns = []
+urlpatterns += router.urls
