b0aea46f26d9f92e3b8b312015686d6ab0d16950
Александра <aleksandrasilina@gmail.com>
2024-11-29 12:03:33 +0300

    add products/admin.py

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from products.models import Product
+
+
+@admin.register(Product)
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "model")
+    list_display_links = ("name",)
+    search_fields = ("name",)
