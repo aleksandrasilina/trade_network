77d4e936e495d903f2129fc739614af391ec12d2
Александра <aleksandrasilina@gmail.com>
2024-11-29 14:02:48 +0300

    add trade_network_level field in sellers/serializers.py

--- sellers/serializers.py
+++ sellers/serializers.py
@@ -1,10 +1,28 @@
 from rest_framework import serializers
+from rest_framework.fields import SerializerMethodField
 
 from sellers.models import Seller
 
 
 class SellerSerializer(serializers.ModelSerializer):
+    trade_network_level = SerializerMethodField(read_only=True)
+
+    def get_trade_network_level(self, seller):
+        """Возвращает уровень в иерархии поставщиков, где 0 - это завод, 1 - следующее звено в цепочке поставок."""
+        if seller.supplier:
+            if seller.supplier.supplier:
+                return 2
+            return 1
+        return 0
 
     class Meta:
         model = Seller
         fields = "__all__"
+
+
+class SellerUpdateSerializer(serializers.ModelSerializer):
+    """Сериалайзер для обновлений. Запрещает обновление через API поля «Задолженность перед поставщиком»."""
+
+    class Meta:
+        model = Seller
+        exclude = ("debt",)
