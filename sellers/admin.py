774a7e429bf23e8ba6e675a2e7c3142ecdecfeea
Александра <aleksandrasilina@gmail.com>
2024-11-29 12:06:06 +0300

    add sellers/admin.py
    register Seller
    add link_to_supplier, clear_debt action

--- sellers/admin.py
+++ sellers/admin.py
@@ -1,3 +1,34 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.urls import reverse
+from django.utils.html import format_html
 
-# Register your models here.
+from sellers.models import Seller
+
+
+@admin.register(Seller)
+class SellerAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'seller_type', 'city', 'link_to_supplier', 'trade_network_level', 'debt')
+    list_filter = ('city', 'seller_type')
+    search_fields = ('name', 'email')
+    list_display_links = ("name",)
+    list_select_related = ('supplier',)
+    actions = ("clear_debt",)
+
+    def link_to_supplier(self, obj):
+        """Возвращает ссылку на поставщика."""
+        if obj.supplier is not None:
+            link = reverse("admin:sellers_seller_change", args=[obj.supplier.id])
+            return format_html(
+                '<a href="{}">{}</a>',
+                link,
+                obj.supplier,
+            )
+
+    link_to_supplier.short_description = 'Поставщик'
+
+    @admin.action(description='Обнулить задолженность')
+    def clear_debt(modeladmin, request, queryset):
+        """Очищает задолженность перед поставщиком."""
+
+        queryset.update(debt=0)
+        messages.success(request, "Задолженность успешно обнулена!")
