526493a5be02c5b07a7e187907815c7d48908e7a
Александра <aleksandrasilina@gmail.com>
2024-12-02 11:31:49 +0300

    deleter permission_classes in users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,4 @@
 from django.urls import path
-from rest_framework.permissions import AllowAny
 from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
@@ -8,14 +7,6 @@ from users.apps import UsersConfig
 app_name = UsersConfig.name
 
 urlpatterns = [
-    path(
-        "login/",
-        TokenObtainPairView.as_view(permission_classes=(AllowAny,)),
-        name="login",
-    ),
-    path(
-        "token/refresh/",
-        TokenRefreshView.as_view(permission_classes=(AllowAny,)),
-        name="token_refresh",
-    ),
+    path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
