efa1cf64e829e07c1c456aaf415cf8c83a62bc3d
Александра <aleksandrasilina@gmail.com>
2024-12-03 13:56:26 +0300

    add UserCreateAPIView path to users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -3,10 +3,12 @@ from rest_framework_simplejwt.views import (TokenObtainPairView,
                                             TokenRefreshView)
 
 from users.apps import UsersConfig
+from users.views import UserCreateAPIView
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="login"),
     path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", UserCreateAPIView.as_view(), name="register"),
 ]
